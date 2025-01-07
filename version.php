6cce0c7be3f04388b66ca8d230d5340a6e7814a0
Philipp Memmel <philipp.memmel@gmail.com>
2025-01-07 11:55:23 +0100

    Fix: Bump version to v0.3.1

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_ai';
 $plugin->release = '0.3.1';
-$plugin->version = 2024070100;
+$plugin->version = 2025010700;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = [
